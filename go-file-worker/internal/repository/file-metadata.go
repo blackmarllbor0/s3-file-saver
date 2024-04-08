78fd03a8c7ddeb18f5a3a444903bd8890c6ceace
blackmarllbor0 <3100194@gmail.com>
2024-04-09 01:42:14 +0300

    feat: add metadata repo interface and impl

--- go-file-worker/internal/repository/file-metadata.go
+++ go-file-worker/internal/repository/file-metadata.go
@@ -8,4 +8,6 @@ import (
 
 type FileMetadataRepo interface {
 	CreateFileMetadata(ctx context.Context, tx pgx.Tx, metadata models.FileMetadata) (ID uint64, err error)
+	CreateManyMetadataRows(ctx context.Context, tx pgx.Tx, metadata []models.FileMetadata) error
+	DeleteFiles(ctx context.Context, tx pgx.Tx, fileNames []string) error
 }
--- go-file-worker/internal/repository/postgres/file-metadata.go
+++ go-file-worker/internal/repository/postgres/file-metadata.go
@@ -5,6 +5,7 @@ import (
 	"app/internal/repository"
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -23,22 +24,61 @@ func (f fileMetadataRepo) CreateFileMetadata(
 ) (ID uint64, err error) {
 	if err := tx.QueryRow(
 		ctx,
-		`INSERT INTO files.files_metadata (size, name, ext, content_type, hash_sum, path, created_by)
-		 VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+		`INSERT INTO files.metadata (size, name, ext, content_type, path)
+		 VALUES ($1, $2, $3, $4, $5)`,
 		metadata.Size,
 		metadata.Name,
 		metadata.Ext,
 		metadata.ContentType,
-		metadata.HashSum,
 		metadata.Path,
-		metadata.CreatedBy,
 	).Scan(&ID); err != nil {
-		if err := tx.Rollback(ctx); err != nil {
-			return 0, fmt.Errorf("pg: failed to cancel transaction: %v", err)
-		}
-
 		return 0, fmt.Errorf("pg: failed to insert metadata: %v", err)
 	}
 
 	return ID, nil
+}
+
+func (f fileMetadataRepo) CreateManyMetadataRows(ctx context.Context, tx pgx.Tx, metadata []models.FileMetadata) error {
+	var queryBuilder strings.Builder
+	queryBuilder.WriteString("INSERT INTO files.metadata (size, name, ext, content_type, path) VALUES")
+
+	args := make([]interface{}, 0)
+
+	for i, file := range metadata {
+		num := i * 5 // num of params per file
+		queryBuilder.WriteString(fmt.Sprintf("($%d, $%d, $%d, $%d, $%d),", num+1, num+2, num+3, num+4, num+5))
+		args = append(args, file.Size, file.Name, file.Ext, file.ContentType, file.Path)
+	}
+
+	// delete last comma
+	queryStr := queryBuilder.String()
+	queryStr = queryStr[:len(queryStr)-1]
+
+	if _, err := tx.Query(ctx, queryStr, args...); err != nil {
+		return fmt.Errorf("pg: failed to batch insert metadata: %v", err)
+	}
+
+	return nil
+}
+
+func (f fileMetadataRepo) DeleteFiles(ctx context.Context, tx pgx.Tx, fileNames []string) error {
+	queryBuilder := strings.Builder{}
+	queryBuilder.WriteString("UPDATE files.metadata SET is_deleted = true WHERE name IN (")
+
+	placeholders := make([]string, len(fileNames))
+	args := make([]interface{}, len(fileNames))
+
+	for i, id := range fileNames {
+		placeholders[i] = fmt.Sprintf("$%d", i+1)
+		args[i] = id
+	}
+
+	queryBuilder.WriteString(strings.Join(placeholders, ", "))
+	queryBuilder.WriteString(")")
+
+	if err := tx.QueryRow(ctx, queryBuilder.String(), args...); err != nil {
+		return fmt.Errorf("pg: failed to batch deleted files: %v", err)
+	}
+
+	return nil
 }
